0e44e2fb285372b68406197c35f0e57e7325f336
simekons <62706897+simekons@users.noreply.github.com>
2022-05-26 16:02:12 +0200

    Esbozado el Player.
    Co-Authored-By: albgom21 <62644841+albgom21@users.noreply.github.com>

---
+++ scripts/Player.cc
@@ -0,0 +1,6 @@
+#include "Player.h"
+
+Player::Player(Texture* txt, Texture* txtShield, Vector2D pos, int16_t num){
+    _txt = txt; _txtShield = txtShield; _pos = pos; _num = num;
+}
+
---
+++ scripts/Player.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "Vector2D.h"
+
+class Texture;
+class Player {
+private:
+    Texture* _txt, *_txtShield;
+    Vector2D _pos;
+    int16_t _num, _numShield;
+
+public:
+    Player(Texture* txt, Texture* txtShield, Vector2D pos, int16_t num);
+    ~Player(){}
+
+    bool canShield() { return (_numShield < 3); }
+
+    void setPos(Vector2D pos) { _pos = pos; }
+    void setNum(int16_t num) { _num = num; }
+    void addShield() { _numShield++; }
+
+    Vector2D getPos() { return _pos; }
+    int16_t getNum() { return _num; }
+
+};
