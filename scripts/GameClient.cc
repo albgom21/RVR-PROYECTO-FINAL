cf7ddf2a232f6adb92922c44ac2955fe4793cc3a
albgom21 <albgom21@ucm.es>
2022-06-01 18:09:35 +0200

    Limpieza código II
    Co-Authored-By: simekons <62706897+simekons@users.noreply.github.com>

--- scripts/GameClient.cc
+++ scripts/GameClient.cc
@@ -7,200 +7,215 @@
 #include "Message.h"
 #include "GOInfo.h"
 
-GameClient::GameClient(const char *s, const char *p, const char *n) : socket(s, p){
-    _app = Game::GetInstance();
-    lastShoot = SDL_GetTicks();
+GameClient::GameClient(const char* s, const char* p, const char* n) : socket(s, p) {
+	_app = Game::GetInstance();
+	lastShoot = SDL_GetTicks();
 
-    for (auto &image : Resources::imageRoutes) 
-        _app->getTextureManager()->loadFromImg(image.id, _app->getRenderer(), image.route);
+	// Cargamos la textura.
+	for (auto& image : Resources::imageRoutes)
+		_app->getTextureManager()->loadFromImg(image.id, _app->getRenderer(), image.route);
 
-    _myPlayer = new Player(_app->getTextureManager()->getTexture(Resources::ID::P1),std::strtol(n, NULL, 10));
+	// Instanciamos el jugador.
+	_myPlayer = new Player(_app->getTextureManager()->getTexture(Resources::ID::P1), std::strtol(n, NULL, 10));
 }
 
 GameClient::~GameClient() {
-    delete _myPlayer;
-    delete _app;
+	delete _myPlayer;
+	delete _app;
 }
 
-void GameClient::initClient(){
-    Message m = Message(MessageType::LOGIN, _myPlayer);
-    if (socket.send(m, socket) == -1)
-        std::cout << "ERROR: no se pudo enviar el mensaje de LOGIN\n";
+void GameClient::initClient() {
+	// Informamos al servidor de que se ha conectado un cliente.
+	Message m = Message(MessageType::LOGIN, _myPlayer);
+	if (socket.send(m, socket) == -1)
+		std::cout << "ERROR: no se pudo enviar el mensaje de LOGIN\n";
 }
 
-void GameClient::render(){
-    SDL_RenderClear(_app->getRenderer());
+void GameClient::render() {
+	SDL_RenderClear(_app->getRenderer());
 
-    //Fondo
-    Texture *t = _app->getTextureManager()->getTexture(Resources::ID::BACKGROUND);
-    t->render({0, 0, W_WIDTH, W_HEIGHT});
+	// RENDER DE TEXTURAS -----------------------------------------------------
+	// Fondo.
+	Texture* t = _app->getTextureManager()->getTexture(Resources::ID::BACKGROUND);
+	t->render({ 0, 0, W_WIDTH, W_HEIGHT });
 
-    //Jugador
-    _myPlayer->getTexture()->render({(int)_myPlayer->getPos().getX(), (int)_myPlayer->getPos().getY(),TAM_JUG_X, TAM_JUG_Y});
+	// Jugador.
+	_myPlayer->getTexture()->render({ (int)_myPlayer->getPos().getX(), (int)_myPlayer->getPos().getY(),TAM_JUG_X, TAM_JUG_Y });
 
-    //Otro jugador cuando se conecte
-    if(other){
-         t = _app->getTextureManager()->getTexture(Resources::ID::P2);
-         t->render({(int)_otherPlayer.pos.getX(), (int)_otherPlayer.pos.getY(), TAM_JUG_X, TAM_JUG_Y});
-    }
+	// Conexión de un nuevo jugador.
+	if (other) {
+		t = _app->getTextureManager()->getTexture(Resources::ID::P2);
+		t->render({ (int)_otherPlayer.pos.getX(), (int)_otherPlayer.pos.getY(), TAM_JUG_X, TAM_JUG_Y });
+	}
 
-    //Escudos
-    t = _app->getTextureManager()->getTexture(Resources::ID::SHIELD);
-    for (auto it = shields.begin(); it != shields.end(); ++it)
-    {
-        GOInfo s = (*it).second;
-        t->render({(int)s.pos.getX(), (int)s.pos.getY(), TAM_SHIELD_X, TAM_SHIELD_Y});
-    }
+	// Escudos.
+	t = _app->getTextureManager()->getTexture(Resources::ID::SHIELD);
+	for (auto it = shields.begin(); it != shields.end(); ++it)
+	{
+		GOInfo s = (*it).second;
+		t->render({ (int)s.pos.getX(), (int)s.pos.getY(), TAM_SHIELD_X, TAM_SHIELD_Y });
+	}
 
-    //Balas
-   t = _app->getTextureManager()->getTexture(Resources::ID::BULLET);
-   for (auto it = bullets.begin(); it != bullets.end(); ++it){
-       GOInfo b = (*it).second;
-       t->render({(int)b.pos.getX(), (int)b.pos.getY(), TAM_SHIELD_Y, TAM_SHIELD_X}); // PONER CONSTANTES
-   }
+	// Balas.
+	t = _app->getTextureManager()->getTexture(Resources::ID::BULLET);
+	for (auto it = bullets.begin(); it != bullets.end(); ++it) {
+		GOInfo b = (*it).second;
+		t->render({ (int)b.pos.getX(), (int)b.pos.getY(), TAM_SHIELD_Y, TAM_SHIELD_X }); // PONER CONSTANTES
+	}
 
-    SDL_RenderPresent(_app->getRenderer());
+	SDL_RenderPresent(_app->getRenderer());
 }
 
 void GameClient::net_thread()
 {
-    while (playing)
-    {
-        Message m;
-        socket.recv(m);
+	while (playing)
+	{
+		Message m;
+		socket.recv(m);
 
-        switch (m.getMessageType())
-        {
-            case MessageType::NEWPLAYER:
-            {
-                GOInfo p = m.getGOInfo();
-                //std::cout<< "MENSAJE N: " << p.nJug<< "\n";
-                //std::cout<< "MYPLAYER N: " << _myPlayer->getNum()<< "\n";
-                if (p.nJug != _myPlayer->getNum()){                   
-                    _otherPlayer = p;
-                    other = true;
-                }
-                else
-                {
-                    _myPlayer->setPos(p.pos);
-                    _myPlayer->setNum(p.nJug);
-                }
+		// RECEPCIÓN DE MENSAJES -----------------------------------------------------
+		switch (m.getMessageType())
+		{
+		// Nuevo jugador conectado.
+		case MessageType::NEWPLAYER:
+		{
+			GOInfo p = m.getGOInfo();
+			if (p.nJug != _myPlayer->getNum()) {
+				_otherPlayer = p;
+				other = true;
+			}
+			else
+			{
+				_myPlayer->setPos(p.pos);
+				_myPlayer->setNum(p.nJug);
+			}
 
-                break;
-            }
-            case MessageType::PLAYERPOS:
-            {
-                GOInfo p = m.getGOInfo();
-                _otherPlayer = p;
+			break;
+		}
+		// Posición de jugador.
+		case MessageType::PLAYERPOS:
+		{
+			GOInfo p = m.getGOInfo();
+			_otherPlayer = p;
 
-                break;
-            }
-            case MessageType::BALAPOS:
-            {
-                GOInfo b = m.getGOInfo();
-                bullets[b.id] = b;
+			break;
+		}
+		// Posición de bala.
+		case MessageType::BALAPOS:
+		{
+			GOInfo b = m.getGOInfo();
+			bullets[b.id] = b;
 
-                break;
-            }
+			break;
+		}
+		// Muerte de jugador.
+		case MessageType::PLAYERDEAD:
+		{
+			if (m.getGOInfo().nJug == _myPlayer->getNum())
+				std::cout << "GAME OVER\n";
+			else
+				std::cout << "YOU WIN!\n";
 
-            case MessageType::PLAYERDEAD:
-            {
-                if (m.getGOInfo().nJug == _myPlayer->getNum())     
-                    std::cout << "GAME OVER\n";                
-                else
-                    std::cout << "YOU WIN!\n";
-
-                m.setMsgType(MessageType::LOGOUT);
-                socket.send(m, socket);
+			m.setMsgType(MessageType::LOGOUT);
+			socket.send(m, socket);
 
 
-                playing = false;
+			playing = false;
 
-                break;
-            }
-            case MessageType::NEWESCUDO:
-            {
-                GOInfo s = m.getGOInfo();
-                if(s.nJug == _myPlayer->getNum())
-                    _myPlayer->addShield();
-                shields[s.id] = s;
-                break;
-            }
-            case MessageType::NEWBALA:
-            {
-                GOInfo b = m.getGOInfo();
-                bullets[b.id] = b;
-                break;
-            }
-
-            case MessageType::BORRABALA:
-            {
-                bullets.erase(m.getGOInfo().id);
-                break;
-            }
-
-            case MessageType::BORRAESCUDO:
-            {
-                GOInfo s = m.getGOInfo();
-                if(s.nJug == _myPlayer->getNum())
-                    _myPlayer->subShield();
-                shields.erase(m.getGOInfo().id);
-                break;
-            }
-        }
-    }
+			break;
+		}
+		// Nuevo escudo.
+		case MessageType::NEWESCUDO:
+		{
+			GOInfo s = m.getGOInfo();
+			if (s.nJug == _myPlayer->getNum())
+				_myPlayer->addShield();
+			shields[s.id] = s;
+			break;
+		}
+		// Nueva bala.
+		case MessageType::NEWBALA:
+		{
+			GOInfo b = m.getGOInfo();
+			bullets[b.id] = b;
+			break;
+		}
+		// Borrar una bala.
+		case MessageType::BORRABALA:
+		{
+			bullets.erase(m.getGOInfo().id);
+			break;
+		}
+		// Borrar un escudo.
+		case MessageType::BORRAESCUDO:
+		{
+			GOInfo s = m.getGOInfo();
+			if (s.nJug == _myPlayer->getNum())
+				_myPlayer->subShield();
+			shields.erase(m.getGOInfo().id);
+			break;
+		}
+		}
+	}
 }
 
 
-void GameClient::input(){
-    bool posM = false;
-    bool escudoM = false;
-    bool balaM = false;
+void GameClient::input() {
+	bool posM = false;
+	bool escudoM = false;
+	bool balaM = false;
 	HandleEvents::instance()->update();
-	if(HandleEvents::instance()->isKeyDown(SDL_SCANCODE_W) && (_myPlayer->getPos().getY()-VELOCITY) >= 0){
-        _myPlayer->setPos({_myPlayer->getPos().getX(), _myPlayer->getPos().getY() - VELOCITY});
-        posM = true;
-    }
-    else if (HandleEvents::instance()->isKeyDown(SDL_SCANCODE_S) && (_myPlayer->getPos().getY()+VELOCITY+TAM_JUG_Y) <= W_HEIGHT){
-        _myPlayer->setPos({_myPlayer->getPos().getX(), _myPlayer->getPos().getY() + VELOCITY});
-        posM = true;
-    }
-    else if (HandleEvents::instance()->isKeyDown(SDL_SCANCODE_X) )
-    {           
-        std::cout<<"ESCUDOS DE JUG: "<<_myPlayer->getNum() <<" -> " <<_myPlayer->getNumShield()<<std::endl;
 
-        if(_myPlayer->canShield())
-            escudoM = true;
-    }
-    else if (HandleEvents::instance()->isKeyDown(SDL_SCANCODE_SPACE) && SDL_GetTicks() - lastShoot > delay)
-    {
-        balaM = true;
-        lastShoot = SDL_GetTicks();
-    }
+	// LECTURA DE INPUT -----------------------------------------------------
+	// Movimiento positivo en el eje vertical.
+	if (HandleEvents::instance()->isKeyDown(SDL_SCANCODE_W) && (_myPlayer->getPos().getY() - VELOCITY) >= 0) {
+		_myPlayer->setPos({ _myPlayer->getPos().getX(), _myPlayer->getPos().getY() - VELOCITY });
+		posM = true;
+	}
+	// Movimiento negativo en el eje vertical.
+	else if (HandleEvents::instance()->isKeyDown(SDL_SCANCODE_S) && (_myPlayer->getPos().getY() + VELOCITY + TAM_JUG_Y) <= W_HEIGHT) {
+		_myPlayer->setPos({ _myPlayer->getPos().getX(), _myPlayer->getPos().getY() + VELOCITY });
+		posM = true;
+	}
+	// Creación de escudo.
+	else if (HandleEvents::instance()->isKeyDown(SDL_SCANCODE_X))
+	{
+		if (_myPlayer->canShield())
+			escudoM = true;
+	}
+	// Creación de bala.
+	else if (HandleEvents::instance()->isKeyDown(SDL_SCANCODE_SPACE) && SDL_GetTicks() - lastShoot > delay)
+	{
+		balaM = true;
+		lastShoot = SDL_GetTicks();
+	}
 
-    //Mandar el mensaje al servidor
-    if (posM && playing)
-    {
-        Message m(MessageType::PLAYERPOS, _myPlayer);
-        socket.send(m, socket);
-    }
-    else if (escudoM && playing)
-    {
-        //std::cout<<"ESCUDOS DE JUG: "<<_myPlayer->getNum() <<" -> " <<_myPlayer->getNumShield()<<std::endl;
-        Message m(MessageType::ESCUDO, _myPlayer);
-        socket.send(m, socket);
-    }
-    else if (balaM && playing)
-    {
-        Message m(MessageType::DISPARO, _myPlayer);
-        socket.send(m, socket);
-    }
+	// ENVÍO DE MENSAJES -----------------------------------------------------
+	// Posición del jugador.
+	if (posM && playing)
+	{
+		Message m(MessageType::PLAYERPOS, _myPlayer);
+		socket.send(m, socket);
+	}
+	// Nuevo escudo.
+	else if (escudoM && playing)
+	{
+		Message m(MessageType::ESCUDO, _myPlayer);
+		socket.send(m, socket);
+	}
+	// Nueva bala.
+	else if (balaM && playing)
+	{
+		Message m(MessageType::DISPARO, _myPlayer);
+		socket.send(m, socket);
+	}
 
 }
 
-void GameClient::run(){
-    while(playing){
-        if(other) input();
-        render();
-    }    
+void GameClient::run() {
+	while (playing) {
+		// Hasta que no se conecte el otro jugador, no empieza a leer el input.
+		if (other) input();
+		render();
+	}
 }
--- scripts/GameClient.h
+++ scripts/GameClient.h
@@ -10,27 +10,44 @@
 class Game;
 class Player;
 
+// Cliente del juego.
 class GameClient {
 private:
+    // Puntero al jugador del propio cliente.
     Player* _myPlayer = nullptr;
-    Game*   _app = nullptr;
-    std::map<int16_t,GOInfo> bullets;
-    std::map<int16_t,GOInfo> shields;
+    // Puntero para iniciar la ventana y la carga de texturas.
+    Game* _app = nullptr;
+    // Vector para las balas.
+    std::map<int16_t, GOInfo> bullets;
+    // Vector para los escudos.
+    std::map<int16_t, GOInfo> shields;
 
+    // Socket del cliente.
     Socket socket;
+    // Bool del bucle de juego.
     bool playing = true;
+    // Bool para saber si hay otro jugador.
     bool other = false;
+    // GOInfo del otro jugador.
     GOInfo _otherPlayer;
 
+    // Cooldown para las balas
     const float delay = 750.0f;
     float lastShoot = 0.0f;
 
-public: 
-    GameClient(const char *s, const char *p, const char *n);
+public:
+    // Constructora del cliente.
+    GameClient(const char* s, const char* p, const char* n);
+    // Destructora del cliente.
     ~GameClient();
+    // Inicialización del cliente.
     void initClient();
-    void render();    
+    // Render del cliente.
+    void render();
+    // Receptor de mensajes.
     void net_thread();
+    // Input del cliente.
     void input();
+    // Bucle principal del cliente.
     void run();
 };
